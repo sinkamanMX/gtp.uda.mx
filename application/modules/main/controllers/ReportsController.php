6c0df9277a55fab6831d9bb137f72e181b53c87f
Enrique Peña Gonzalez <er.penagonzalez@gmail.com>
2014-07-23 13:52:18 -0500

    Update
    Update

--- application/modules/main/controllers/ReportsController.php
+++ application/modules/main/controllers/ReportsController.php
@@ -1,15 +1,53 @@
 <?php
 
-class main_ReportsController extends My_Controller_Action
+class main_reportsController extends My_Controller_Action
 {	
+	protected $_clase = 'reports';
+	public $validateNumbers;
+	public $validateAlpha;
+	public $dataIn;
+	public $idToUpdate=-1;
+	public $errors = Array();
+	public $operation='init';
+	public $resultop=null;	
+		
     public function init()
     {
-		$this->view->layout()->setLayout('layout_blank');
-		
+    	try{
 		$sessions = new My_Controller_Auth();
-        if($sessions->validateSession()){
-	        $this->view->dataUser   = $sessions->getContentSession();   		
+		$perfiles = new My_Model_Perfiles();
+        if(!$sessions->validateSession()){
+            $this->_redirect('/');		
 		}
+		$this->view->dataUser   = $sessions->getContentSession();
+		$this->view->modules    = $perfiles->getModules($this->view->dataUser['ID_PERFIL']);
+		$this->view->moduleInfo = $perfiles->getDataModule($this->_clase);
+		$this->view->idEmpresa  = $this->view->dataUser['ID_EMPRESA'];
+		
+		$this->dataIn = $this->_request->getParams();
+		$this->validateNumbers = new Zend_Validate_Digits();
+				
+		if(isset($this->dataIn['optReg'])){
+			$this->operation = $this->dataIn['optReg'];
+			
+			if($this->operation=='update'){
+				$this->operation = $this->dataIn['optReg'];
+
+				$this->validateAlpha   = new Zend_Validate_Alnum(array('allowWhiteSpace' => true));				
+			}	
+		}
+		
+		if(isset($this->dataIn['catId']) && $this->validateNumbers->isValid($this->dataIn['catId'])){
+			$this->idToUpdate 	   = $this->dataIn['catId'];	
+		}else{
+			$this->idToUpdate 	   = -1;
+			$this->errors['status'] = 'no-info';
+		}	
+
+		} catch (Zend_Exception $e) {
+            echo "Caught exception: " . get_class($e) . "\n";
+        	echo "Message: " . $e->getMessage() . "\n";                
+        }  		
     }
     
     public function indexAction(){
